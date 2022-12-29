370e5ade57b1aee349f18d238412cc01ba6e4eff
Alex Swanson <swanson5467@gmail.com>
2022-12-28 20:51:01 -0500

    Create playRound function

--- index.js
+++ index.js
@@ -8,3 +8,16 @@ function getComputerChoice() {
         return "Scissors";
     }
 }
+
+
+function playRound(playerSelection, computerSelection) {
+    if ( (playerSelection.toUpperCase() == "ROCK" && computerSelection == "Scissors") || (playerSelection.toUpperCase() == "PAPER" && computerSelection == "Rock") 
+    || (playerSelection.toUpperCase() == "SCISSORS" && computerSelection == "Paper") ) {
+        return "Congrats! You win!";
+    } else if ((playerSelection.toUpperCase() == "PAPER" && computerSelection == "Scissors") || (playerSelection.toUpperCase() == "SCISSORS" && computerSelection == "Rock") 
+    || (playerSelection.toUpperCase() == "ROCK" && computerSelection == "Paper") ) {
+        return "Aww that's too bad! You lose!";
+    } else {
+        return "It's a Draw!";
+    }
+}
