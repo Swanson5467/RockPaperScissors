cbe5eb0dc4fd5d5e6e92a6f06d4855bdcb98baf5
Alex Swanson <swanson5467@gmail.com>
2023-02-23 19:52:44 -0500

    Create four buttons for each choice and one for reset. Implement basic functionality with event handlers to play game.

--- index.js
+++ index.js
@@ -10,42 +10,73 @@ function getComputerChoice() {
 }
 
 
+let playerWinCount = 0;
+let computerWinCount = 0;
+let drawCount = 0;
+
+
 function playRound(playerSelection, computerSelection) {
     if ( (playerSelection.toUpperCase() != "ROCK") && (playerSelection.toUpperCase() != "PAPER") && (playerSelection.toUpperCase() != "SCISSORS") ) {
-        console.log("Error! Please make a valid selection!");
+        alert("Error! Please make a valid selection!");
         return "error";
     } else if ( (playerSelection.toUpperCase() == "ROCK" && computerSelection == "Scissors") || (playerSelection.toUpperCase() == "PAPER" && computerSelection == "Rock") 
     || (playerSelection.toUpperCase() == "SCISSORS" && computerSelection == "Paper") ) {
-        console.log("Congrats! You win this round!");
+        alert(`The computer has chosen ${computerSelection}...`);
+        playerWinCount += 1;
+        playerWins.textContent = playerWinCount;
+        //alert("Congrats! You win this round!");
         return true;
     } else if ((playerSelection.toUpperCase() == "PAPER" && computerSelection == "Scissors") || (playerSelection.toUpperCase() == "SCISSORS" && computerSelection == "Rock") 
     || (playerSelection.toUpperCase() == "ROCK" && computerSelection == "Paper") ) {
-        console.log("Aww that's too bad! You lost this round!");
+        alert(`The computer has chosen ${computerSelection}...`);
+        computerWinCount += 1;
+        computerWins.textContent = computerWinCount;
+        //alert("Aww that's too bad! You lost this round!");
         return false;
     } else {
-        console.log("This round ends in a draw!");
+        alert(`The computer has chosen ${computerSelection}...`);
+        drawCount += 1;
+        draws.textContent = drawCount;
+        //alert("This round ends in a draw!");
         return "draw";
     }
 }
 
 
+
+
 function game() {
     let playerWinCount = 0;
     let computerWinCount = 0;
-    for (i = 0; i < 5; i++) {
-        let result = playRound(prompt("Welcome. Please choose rock, paper, or scissors:"), getComputerChoice());
-        if (result == true) {
-            playerWinCount++
-        } else if (result == false) {
-            computerWinCount++
-        } else if (result == "error") {
-            i--;
-        }
-    } if (playerWinCount > computerWinCount) {
-        console.log('the player has won the GAME');
+    playRound(prompt("Welcome. Please choose rock, paper, or scissors:"), getComputerChoice());
+    if (playerWinCount > computerWinCount) {
+        alert('The player has won the GAME');
     } else if (computerWinCount > playerWinCount) {
-        console.log('the computer has won the GAME');
+        alert('The computer has won the GAME');
     } else {
-        console.log('the GAME ends in a draw..');
+        alert('The GAME ends in a draw');
     }
+}
+
+
+const playerWins = document.querySelector('.playerWinText');
+const computerWins = document.querySelector('.computerWinText');
+const draws = document.querySelector('.drawText');
+
+
+const rockButton = document.querySelector('.rock');
+const paperButton = document.querySelector('.paper');
+const scissorsButton = document.querySelector('.scissors');
+const resetButton = document.querySelector('.reset');
+
+rockButton.onclick = () => playRound('rock', getComputerChoice());
+paperButton.onclick = () => playRound('paper', getComputerChoice());
+scissorsButton.onclick = () => playRound('scissors', getComputerChoice());
+resetButton.onclick = function() {
+    playerWinCount = 0;
+    computerWinCount = 0;
+    drawCount = 0;
+    playerWins.textContent = playerWinCount;
+    computerWins.textContent = computerWinCount;
+    draws.textContent = drawCount;
 }
